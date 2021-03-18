66e6139c6f8bae162aa682e808cb65a3808b62c4
Noah K <noah@spacemule.net>
2021-03-19 00:33:41 +0200

    Made changes needed for opensuse (DISPLAY=unix:0), shouldn't affect Ubuntu. Not entirely sure. Testing would be appreciated.

--- lxdesktop-session.sh
+++ lxdesktop-session.sh
@@ -22,19 +22,20 @@ lxc config device add $1 PASocket proxy connect="unix:/run/user/$(id -u)/pulse/n
 lxc config device add $1 mygpu gpu
 
 lxc start $1
+#Adjust as needed. Very likely a better way to do this. Haven't looked into it yet.
 sleep 2
 
 #Only needs to be done once, but left in for safety
 lxc exec $1 -- sed -i "s/; enable-shm = yes/enable-shm = no/g" /etc/pulse/client.conf
 
 #Start the desktop. Needs unbuffer to keep display manager from killing X
-unbuffer lxc exec $1 -t -- sudo -u $2 --login env PULSE_SERVER=unix:/home/.pulse-native XDG_SESSION_TYPE=x11 DISPLAY=:0 $3
+unbuffer lxc exec $1 -t -- sudo -u $2 --login env PULSE_SERVER=unix:/home/.pulse-native XDG_SESSION_TYPE=x11 DISPLAY=unix:0 $3
 
 #Examples of what this command can look like
 
-#unbuffer lxc exec $1 -t -- sudo -u $2 --login env PULSE_SERVER=unix:/home/.pulse-native XDG_SESSION_TYPE=x11 DISPLAY=:0 startxfce4
-#unbuffer lxc exec $1 -t -- sudo -u $2 --login env PULSE_SERVER=unix:/home/.pulse-native XDG_SESSION_TYPE=x11 DISPLAY=:0 budgie-desktop
-#unbuffer lxc exec $1 -t -- sudo -u $2 --login env PULSE_SERVER=unix:/home/.pulse-native XDG_SESSION_TYPE=x11 DISPLAY=:0 dbus-run-session gnome-session --session=ubuntu
+#unbuffer lxc exec $1 -t -- sudo -u $2 --login env PULSE_SERVER=unix:/home/.pulse-native XDG_SESSION_TYPE=x11 DISPLAY=unix:0 startxfce4
+#unbuffer lxc exec $1 -t -- sudo -u $2 --login env PULSE_SERVER=unix:/home/.pulse-native XDG_SESSION_TYPE=x11 DISPLAY=unix:0 budgie-desktop
+#unbuffer lxc exec $1 -t -- sudo -u $2 --login env PULSE_SERVER=unix:/home/.pulse-native XDG_SESSION_TYPE=x11 DISPLAY=unix:0 dbus-run-session gnome-session --session=ubuntu
 
 #Shut down the container and remove its access to host devices/services
 lxc stop $1
